c1615da3f1c372aaff037c8666081fb5397effc3
xana2333 <68159785+xana2333@users.noreply.github.com>
2026-04-03 18:20:34 +0800

    回滚 移除增加的min-width
    移除增加的min-width
    插入的1px高div宽度改为浏览器宽度的0.7

--- finance.inquiry.system.js
+++ finance.inquiry.system.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SCUT财务系统UI优化-财务查询
-// @namespace    http://github.com/xana2333/scut-finance-ui-fix
-// @version      1.3
+// @namespace    http://tampermonkey.net/
+// @version      1.4
 // @description  解决查询系统首页大分辨率情况下UI错位、经费信息需要横向移动才能查看的问题
 // @author       XANA
 // @match        http://202.38.194.48:8182/*
@@ -17,6 +17,7 @@
 
     //获取当前URL
     const currentUrl = window.location.href;
+    //console.log("currentUrl",currentUrl)
     // 获取当前窗口的视口宽度（包含滚动条）
     let window_width = window.innerWidth;
     console.log("Url"+currentUrl+" 当前宽度为: " + window_width + "px");
@@ -28,8 +29,7 @@
         // 这会覆盖 .main 类的 width: 980px;
         GM_addStyle(`
         .main {
-            min-width: 980px !important; //增加最小宽度（与原来保持一致），避免它过小带来新问题
-            width: auto !important; // 或者可以使用百分比, 如 100%, 但 auto 通常更灵活 
+            width: auto !important; // 或者可以使用百分比, 如 100%, 但 auto 通常更灵活
             max-width: none !important; // 移除可能存在的最大宽度限制
             margin-left: auto !important; // 保持居中或根据需要调整
             margin-right: auto !important; // 保持居中或根据需要调整
@@ -38,7 +38,6 @@
         // 这会覆盖 .width 类的 width: 960px;
         GM_addStyle(`
         .width {
-            min-width: 960px !important; //增加最小宽度（与原来保持一致），避免它过小带来新问题
             width: 100% !important; // 改为占据其容器的100%宽度
             max-width: none !important; // 移除可能存在的最大宽度限制
         }
@@ -52,7 +51,7 @@
             // 精确匹配 id 为 div_mb 的元素
             const target = document.getElementById('div_mb');
             if (target) {
-                let val =window_width/2+200;
+                let val =window_width*0.7;
                 let customWidth=val+ "px";
                 const htmlString = '<div class="all_content" style="width:' + customWidth + '; height: 1px;"></div>';
 
