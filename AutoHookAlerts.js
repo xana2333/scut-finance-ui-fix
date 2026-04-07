6cd7495227c2065dd7069a9c314e871adff4f590
xana2333 <68159785+xana2333@users.noreply.github.com>
2026-04-07 17:26:49 +0800

    Update AutoHookAlerts.js

--- AutoHookAlerts.js
+++ AutoHookAlerts.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Hook Alerts in All Frames
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  自动覆盖 我的票夹页面 alert/confirm/prompt 并在调用时进入 debugger，适用于所有 iframe，包括动态加载
 // @author       XANA
-// @match        http://wsyy.cw.scut.edu.cn/hnlgwsyy60/Modules/WDPJ/WDPJ0.aspx*
-// @match        https://wsyy.cw.scut.edu.cn/hnlgwsyy60/Modules/WDPJ/WDPJ0.aspx*
-// @match        http://wsyy-cw.webvpn.scut.edu.cn/hnlgwsyy60/Modules/WDPJ/WDPJ0.aspx*
-// @match        https://wsyy-cw.webvpn.scut.edu.cn/hnlgwsyy60/Modules/WDPJ/WDPJ0.aspx*
+// @match        http://wsyy.cw.scut.edu.cn/*
+// @match        https://wsyy.cw.scut.edu.cn/*
+// @match        http://wsyy-cw.webvpn.scut.edu.cn/*
+// @match        https://wsyy-cw.webvpn.scut.edu.cn/*
 // @grant        none
 // ==/UserScript==
 
@@ -15,23 +15,25 @@
     'use strict';
 
     console.log("[Tampermonkey] Hook Alerts script started");
+    console.log("window.frames.length:",window.frames.length);
 
     /** Hook 目标窗口的 alert/confirm/prompt */
     function hookWindow(win, frameName) {
         try {
             if (win._alertHooked) return; // 避免重复 Hook
+            console.log("currentUrl:", win.location.href," ,frameName:",frameName);
             win.alert = function (message) {
-                console.log("Alert in frame:", frameName, " , message:", message);
-                //debugger;
+                console.log("Alert in frame:", frameName, " ,currentUrl:", win.location.href," , message:", message);
+                debugger;
             };
             win.confirm = function (message) {
-                console.log("Confirm in frame:", frameName, " , message:", message);
-                //debugger;
+                console.log("Confirm in frame:", frameName, " ,currentUrl:", win.location.href," , message:", message);
+                debugger;
                 return true; // 或根据需要返回 false
             };
             win.prompt = function (message, defaultValue) {
-                console.log("Prompt in frame:", frameName, " , message:", message);
-                //debugger;
+                console.log("Prompt in frame:", frameName, " ,currentUrl:", win.location.href," , message:", message);
+                debugger;
                 return defaultValue;
             };
             win._alertHooked = true;
@@ -54,6 +56,7 @@
 
     // 动态检测新出现的 iframe
     setInterval(() => {
+        //console.log("window.frames.length:",window.frames.length);
         hookAllFrames();
     }, 1000);
 
