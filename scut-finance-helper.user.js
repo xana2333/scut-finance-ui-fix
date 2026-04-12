74493ef790ae2cf8214dace4ea17df4d8ed755a6
xana2333 <68159785+xana2333@users.noreply.github.com>
2026-04-12 23:36:14 +0800

    调整 税票录入 页面绑定发票表格iframe高度问题 运行逻辑 更新版本号

--- scut-finance-helper.user.js
+++ scut-finance-helper.user.js
@@ -2,7 +2,7 @@
 // @name         SCUT Finance Helper
 // @name:zh      SCUT财务系统小助手
 // @namespace    https://github.com/xana2333/scut-finance-ui-fix
-// @version      1.0.13
+// @version      1.0.14
 // @description  SCUT网上报账系统 & 财务查询系统辅助小工具：UI修正功能、自动化批量操作，让报账更高效流畅。
 // @author       XANA
 // @homepage     https://github.com/xana2333/scut-finance-ui-fix
